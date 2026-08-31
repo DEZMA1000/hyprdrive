cf435cc1c0e21e06f13bb9338c512d7c88d41613
DEZMA1000 <177497585+DEZMA1000@users.noreply.github.com>
2026-08-30 23:42:30 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -57,28 +57,42 @@ dnf5 install -y \
 
 ### Razer Blade control
 
-git clone --depth=1 \
+# Pin to known-good revision tested on this Blade 16
+RAZERCTL_COMMIT="7a8980671cfce1abe7f37405071509c04385fc04"
+
+git clone \
     https://github.com/TimandXiyu/blade-cli.git \
     /tmp/blade-cli
 
+git -C /tmp/blade-cli checkout "$RAZERCTL_COMMIT"
+
 make -C /tmp/blade-cli
 
+# Install into immutable image-owned paths
 install -Dm755 \
     /tmp/blade-cli/razerctl \
-    /usr/local/bin/razerctl
+    /usr/bin/razerctl
 
 install -Dm755 \
     /tmp/blade-cli/razerctld \
-    /usr/local/bin/razerctld
+    /usr/bin/razerctld
 
 install -Dm644 \
     /tmp/blade-cli/razerctl.1 \
-    /usr/local/share/man/man1/razerctl.1
+    /usr/share/man/man1/razerctl.1
 
+# Install systemd unit into vendor unit directory
 install -Dm644 \
     /tmp/blade-cli/razerctld.service \
-    /etc/systemd/system/razerctld.service
+    /usr/lib/systemd/system/razerctld.service
 
+# Upstream service expects /usr/local/bin.
+# Hyprdrive installs razerctld into /usr/bin instead.
+sed -i \
+    's|ExecStart=/usr/local/bin/razerctld|ExecStart=/usr/bin/razerctld|' \
+    /usr/lib/systemd/system/razerctld.service
+
+# Restore Blade settings after suspend/resume
 install -Dm755 \
     /tmp/blade-cli/zz-razerctld-resume \
     /usr/lib/systemd/system-sleep/zz-razerctld-resume
